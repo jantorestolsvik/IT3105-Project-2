2d18f4cf971816ef9e9b9919093d08e4aefe56c1
jantorestolsvik <jan.tore@stoelsvik.no>
2013-10-21 17:39:56 +0200

    test

--- src/main/java/no/ntnu/gps/main/App.java
+++ src/main/java/no/ntnu/gps/main/App.java
@@ -15,10 +15,10 @@ public class App
     {
 //        ConstraintBasedLocalSearch temp = new GraphColorMC("graph.txt", 4);
         //ConstraintBasedLocalSearch temp = new SimulatedAnnealing(new GraphColorStateManager("graph.txt", 4));
-        ConstraintBasedLocalSearch temp = new SimulatedAnnealing(new KQueensStateManager(10));
+        ConstraintBasedLocalSearch temp = new SimulatedAnnealing(new GraphColorStateManager("graph.txt", 4));
         AbstractState result = temp.solve();
-        System.out.println(result);
-        //GraphColorState result2 = (GraphColorState)result;
-        //result2.display();
+//        System.out.println(result);
+        GraphColorState result2 = (GraphColorState)result;
+        result2.display();
     }
 }
