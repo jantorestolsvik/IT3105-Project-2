521957fbfbe412759361d97ac2c8298fcc5ef06f
jantorestolsvik <jan.tore@stoelsvik.no>
2013-10-21 22:19:00 +0200

    added solved function to futoshiki

--- src/main/java/no/ntnu/gps/states/FutoshikiState.java
+++ src/main/java/no/ntnu/gps/states/FutoshikiState.java
@@ -11,10 +11,42 @@ import java.util.List;
  * @author Jani
  */
 public class FutoshikiState extends AbstractState {
+   
+    private int board[];
+    private int [][] smallerThanOperators;
+    private int rows;
+    public FutoshikiState(int rows) {
+        board = new int[rows*rows];
+        smallerThanOperators = new int [10][2];
+        this.rows = rows;
+    }
 
     @Override
     public boolean solved() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (int i = 0; i < smallerThanOperators.length; i++) {
+            if (smallerThanOperators[i][0] > smallerThanOperators[i][1]) {
+                return false;
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            boolean []seenRow = new boolean[rows];
+            for (int j = 0; j < rows; j++) {
+                if (seenRow[board[i*rows + j]] == true) {
+                    return false;
+                } else {
+                    seenRow[board[i*rows + j]] = true;
+                }
+            }
+            boolean []seenCol = new boolean[rows];
+            for (int j = 0; j < rows; j++) {
+                if (seenCol[board[i + j*rows]] == true) {
+                    return false;
+                } else {
+                    seenCol[board[i + j*rows]] = true;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
