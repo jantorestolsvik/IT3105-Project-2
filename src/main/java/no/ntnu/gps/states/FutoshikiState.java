eecaeebae4fed8c7e6eed2f7261ef249710ffbdd
Ethol <tomeivin@sutd.ntnu.no>
2013-10-22 02:58:13 +0200

    futoshiki methods implemented, allthough badly.

--- src/main/java/no/ntnu/gps/states/FutoshikiState.java
+++ src/main/java/no/ntnu/gps/states/FutoshikiState.java
@@ -7,68 +7,231 @@ package no.ntnu.gps.states;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.text.ChangedCharSetException;
+
 /**
  *
  * @author Jani
  */
 public class FutoshikiState extends AbstractState {
-   
-    private int board[];
-    private int [][] smallerThanOperators;
-    private int rows;
-    public FutoshikiState(int rows) {
-        board = new int[rows*rows];
-        smallerThanOperators = new int [0][2];
-        this.rows = rows;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows; j++) {
-                board[i*rows+j] = j;
-            }
-        }
-    }
 
-    @Override
-    public boolean solved() {
-        for (int i = 0; i < rows; i++) {
-            boolean [] seen = new boolean[rows];
-            for (int j = 0; j < rows; j++) {
-                if (seen[board[i+rows*j]] == true) {
-                    return false;
-                } else {
-                    seen[board[i+rows*j]] = true;
-                }
-            }
-        }
-        return true;
-    }
+	private int board[][];
+	private int [][] smallerThanOperators;
+	private int rows;
+	public FutoshikiState(int rows) {
+		board = new int[rows][rows];
+		smallerThanOperators = new int [rows][rows+rows];
+		this.rows = rows;
+		// test eksample
+		
+		
+		smallerThanOperators[0][0] = -1;
+		smallerThanOperators[1][1] = -1;
+		smallerThanOperators[4][1] = -1;
+		smallerThanOperators[2][4] = -1;
+		smallerThanOperators[3][4] = 1;
+		smallerThanOperators[0][5] = -1;
+		smallerThanOperators[4][7] = 1;
+		board =new int[][]{ 
+				{5,5,5,5,5},
+				{5,5,5,5,5},
+				{5,5,5,5,5},
+				{5,5,5,5,5},
+				{5,5,5,5,5}
+		};
+		//solution
+//		board =new int[][]{ 
+//				{5,4,3,2,1},
+//				{4,2,1,5,3},
+//				{1,3,5,4,2},
+//				{3,5,2,1,4},
+//				{2,1,4,3,5}
+//		};
+//		smallerThanOperators[1][0] = 1;
+//		smallerThanOperators[4][5] = -11;
+//		smallerThanOperators[3][6] = 1;
+//		smallerThanOperators[1][7] = 1;
+//		smallerThanOperators[3][8] = -1;
+//		board =new int[][]{ {2,4,1,5,3},
+//				{1,5,3,2,4},
+//				{3,2,5,4,1},
+//				{4,3,2,1,5},
+//				{5,1,4,3,2}
+//		};
 
-    @Override
-    public int evaluation() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	}
 
-    @Override
-    public List<Integer> conflictedStates() {
-    }
+	@Override
+	public boolean solved() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < rows; j++) {
+				if(board[j][i]==0)
+					return false;
+			}
+		}
+		//rows
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < rows; j++) {
+				for (int j2 = j+1; j2 < rows; j2++) {
+					if(board[j][i]==board[j2][i])
+						return false;
 
-    @Override
-    public List<Integer> leastConflictedNeighbours(int chosenIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+				}
+			}
+		}
+		//columns
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < rows; j++) {
+				for (int j2 = j+1; j2 < rows; j2++) {
+					if(board[i][j]==board[i][j2])
+						return false;
 
-    @Override
-    public void change(int chosenIndex, int chosenNeighbour) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+				}
+			}
+		}
+		//inequalities
+//		System.out.println("in");
+		for (int i = 0; i < rows+rows-1; i+=2) {
+			//rows
+			for (int j = 0; j < rows-1; j++) {
+				if(smallerThanOperators[j][i]==0){
 
-    @Override
-    public AbstractState randomNeighbourState() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+				}else if(smallerThanOperators[j][i]<0){
+//					System.out.println("wat");
+					if(board[j][i/2]<board[j+1][i/2])
+						return false;
+				}else{
+//					System.out.println("wat2");
+					if(board[j][i/2]>board[j+1][i/2])
+						return false;
+				}
+			}
+		}
+		for (int i = 1; i < rows+rows-1; i+=2) {
+			//columns
+			for (int j = 0; j < rows; j++) {
+				if(smallerThanOperators[j][i]==0){
 
-    @Override
-    public void display() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+				}else if(smallerThanOperators[j][i]<0){
+//					System.out.println("wat3");
+					if(board[i/2][i/2]<board[i/2][i/2+1])
+						return false;
+				}else{
+//					System.out.println("wat4");
+					if(board[i/2][i/2]>board[i/2][i/2+1])
+						return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int evaluation() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public List<Integer> conflictedStates() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public List<Integer> leastConflictedNeighbours(int chosenIndex) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	/**
+	 * chosen index is the id of the position where 0 would be row 0 column 0, and 6 would be row 1 column 2 if the board is 5x5
+	 * chosen neightbeighbour is the nr to change to.
+	 */
+	public void change(int chosenIndex, int chosenNeighbour) {
+		if(chosenIndex>rows*rows-1||chosenNeighbour>rows){
+			System.err.println("Error the nr is to big");
+			return;
+		}
+		int x = chosenIndex%rows, y = chosenIndex/rows;
+		board[x][y] = chosenNeighbour;
+//		System.out.println(x+":" + y);
+				
+
+	}
+
+	@Override
+	public AbstractState randomNeighbourState() {
+		FutoshikiState returner = this.clone();
+		int pos = (int) (Math.random()*rows*rows);
+		int nr = (int) (Math.random()*rows)+1;
+		returner.change(pos, nr);
+		System.out.println(pos);
+		System.out.println(nr);
+		return returner;
+		
+	}
+	public FutoshikiState clone(){
+		FutoshikiState returner = new FutoshikiState(rows);
+		//yes this is redicules, but nessesary
+		for (int i = 0; i < board.length; i++) {
+			returner.board[i] = this.board[i].clone();
+			
+		}
+		returner.smallerThanOperators = this.smallerThanOperators;
+		
+		return returner;
+	}
+
+	@Override
+	public void display() {
+		String line;
+		for (int i = 0; i < rows-1; i++) {
+			line ="";
+			for (int j = 0; j < rows-1; j++) {
+				line += " " + board[j][i] + " ";
+				if(smallerThanOperators[j][i*2]==0){
+					line += "   ";				
+				}else if(smallerThanOperators[j][i*2]<0){
+					line += " > ";
+				}else{
+					line += " < ";
+				}
+			}
+			System.out.println(line + board[rows-1][i]);
+			line = "";
+			for (int j = 0; j < smallerThanOperators.length; j++) {
+				if(smallerThanOperators[j][i*2+1]==0){
+					line += "      ";				
+				}else if(smallerThanOperators[j][i*2+1]<0){
+					line += "\\/    ";
+				}else{
+					line += "/\\    ";
+				}
+			}
+			System.out.println(line);
+		}
+		line = "";
+		for (int j = 0; j < rows-1; j++) {
+			line += " " + board[j][rows-1] + " ";
+			if(smallerThanOperators[j][rows+rows-2]==0){
+				line += "   ";				
+			}else if(smallerThanOperators[j][rows+rows-2]<0){
+				line += " > ";
+			}else{
+				line += " < ";
+			}
+		}
+		System.out.println(line + board[rows-1][rows-1]);
+
+	}
+	public static void main(String[] args) {
+		FutoshikiState fs = new FutoshikiState(5);
+		FutoshikiState fs2 = new FutoshikiState(5);
+		for (int i = 0; i < 100; i++) {
+			
+			fs = (FutoshikiState) fs.randomNeighbourState();
+		}
+		fs.display();
+		System.out.println(fs.solved());
+	}
+
 }
--- src/main/java/no/ntnu/gps/states/KQueenState.java
+++ src/main/java/no/ntnu/gps/states/KQueenState.java
@@ -12,155 +12,156 @@ import java.util.List;
  * @author Jani
  */
 public class KQueenState extends AbstractState {
-    private int [] conflicts;
-    private int [] queens;
-    private int k;
+	private int [] conflicts;
+	private int [] queens;
+	private int k;
 
-    public KQueenState(int k) {
-        this.k = k;
-        this.conflicts = new int[k];
-        this.queens = new int[k];
-        for (int i = 0; i < k; i++) {
-            queens[i]=0;
-            conflicts[i]=k-1;
-        }
-    }
+	public KQueenState(int k) {
+		this.k = k;
+		this.conflicts = new int[k];
+		this.queens = new int[k];
+		for (int i = 0; i < k; i++) {
+			queens[i]=0;
+			conflicts[i]=k-1;
+		}
+	}
 
-    public KQueenState randomNeighbourState(){
-        KQueenState returner = this.clone();
-        int queen = (int) (Math.random()*k);
-        int pos = (int) (Math.random()*k);
-        while(queens[queen] == pos) {
-            pos = (int) (Math.random()*k);
-        }
-        returner.change(queen, pos);
-        return returner;
-    }
-    
-    public KQueenState clone(){
-        KQueenState returner = new KQueenState(k);
-        returner.conflicts = this.conflicts.clone();
-        returner.queens = this.queens.clone();
-        returner.k = this.k;
-        return returner;
-    }
+	public KQueenState randomNeighbourState(){
+		KQueenState returner = this.clone();
+		int queen = (int) (Math.random()*k);
+		int pos = (int) (Math.random()*k);
+		while(queens[queen] == pos) {
+			pos = (int) (Math.random()*k);
+		}
+		returner.change(queen, pos);
+		return returner;
+	}
 
-    public boolean solved() {
-        for (int i = 0; i < k; i++) {
-            if (conflicts[i] != 0) {
-                    return false;
-            }
-        }
-        return true;
-    }
-        
-    private void addConflict(int queenIndex, int conflict) {
-        for (int i = 0; i < k; i++) {
-            if (i!=queenIndex) {
-                if (queens[i]==queens[queenIndex]) {
-                        conflicts[i]+=conflict;
-                        conflicts[queenIndex]+=conflict;
-                }
-                int offsetPos = Math.abs(queenIndex-i);
-                if (queens[i]==queens[queenIndex]+offsetPos) {
-                        conflicts[i]+=conflict;
-                        conflicts[queenIndex]+=conflict;
-                }
-                if (queens[i]==queens[queenIndex]-offsetPos) {
-                        conflicts[i]+=conflict;
-                        conflicts[queenIndex]+=conflict;
-                }
-            }
-        }
-    }
+	public KQueenState clone(){
+		KQueenState returner = new KQueenState(k);
+		returner.conflicts = this.conflicts.clone();
+		returner.queens = this.queens.clone();
+		returner.k = this.k;
+		return returner;
+	}
 
-    public List<Integer> conflictedStates() {
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < k; i++) {
-                if (conflicts[i] != 0) {
-                        list.add(i);
-                }
-        }
-        return list;
-    }
-        
-    public String toString() {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < k; i++) {
-                    for (int j = 0; j < k; j++) {
-                            if (queens[i]==j) {
-                                    sb.append("Q"+conflicts[i]+"\t");
-                            } else {
-                                    sb.append("\t");
-                            }
-                    }
-                    sb.append("\n");
-            }
-            return sb.toString();
-    }
+	public boolean solved() {
+		for (int i = 0; i < k; i++) {
+			if (conflicts[i] != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public int[] getConflicts() {
-            return conflicts;
-    }
+	private void addConflict(int queenIndex, int conflict) {
+		for (int i = 0; i < k; i++) {
+			if (i!=queenIndex) {
+				if (queens[i]==queens[queenIndex]) {
+					conflicts[i]+=conflict;
+					conflicts[queenIndex]+=conflict;
+				}
+				int offsetPos = Math.abs(queenIndex-i);
+				if (queens[i]==queens[queenIndex]+offsetPos) {
+					conflicts[i]+=conflict;
+					conflicts[queenIndex]+=conflict;
+				}
+				if (queens[i]==queens[queenIndex]-offsetPos) {
+					conflicts[i]+=conflict;
+					conflicts[queenIndex]+=conflict;
+				}
+			}
+		}
+	}
 
-    @Override
-    public int evaluation() {
-        int eval = 0;
-        if (this.solved()){
-                return Integer.MAX_VALUE;
-        }
-        for (int i = 0; i < conflicts.length; i++) {
-                eval += conflicts[i];
-        }
-        return conflicts.length*conflicts.length - eval;
-    }
+	public List<Integer> conflictedStates() {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < k; i++) {
+			if (conflicts[i] != 0) {
+				list.add(i);
+			}
+		}
+		return list;
+	}
 
-    @Override
-    public List<Integer> leastConflictedNeighbours(int chosenIndex) {
-        int min = Integer.MAX_VALUE;
-        List<Integer> list = new ArrayList<Integer>();
-        int [] tempConflicts = new int[k];
-        for (int i = 0; i < k; i++) {
-                for (int j = 0; j < k; j++) {
-                        if (j!=chosenIndex) {
-                                if (i==queens[j]) {
-                                        tempConflicts[i]++;
-                                }
-                                int offsetPos = Math.abs(j-chosenIndex);
-                                if (i==queens[j]+offsetPos) {
-                                        tempConflicts[i]++;
-                                }
-                                if (i==queens[j]-offsetPos) {
-                                        tempConflicts[i]++;
-                                }
-                        }
-                }
-        }
-        for (int i = 0; i < k; i++) {
-                if (tempConflicts[i] < min) {
-                        min = tempConflicts[i];
-                }
-        }
-        for (int i = 0; i < k; i++) {
-                if(tempConflicts[i] == min) {
-                        list.add(i);
-                }
-        }
-        if (list.contains(queens[chosenIndex])&&(list.size()!=1)) {
-                list.remove(list.indexOf(queens[chosenIndex]));
-        }
-        return list;
-    }
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j < k; j++) {
+				if (queens[i]==j) {
+					sb.append("Q"+conflicts[i]+"\t");
+				} else {
+					sb.append("\t");
+				}
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
 
-    @Override
-    public void change(int chosenIndex, int chosenNeighbour) {
-        addConflict(chosenIndex, -1);
-        queens[chosenIndex] = chosenNeighbour;
-        addConflict(chosenIndex, 1);
-    }
+	public int[] getConflicts() {
+		return conflicts;
+	}
 
-    @Override
-    public void display() {
-        System.out.println(this.toString());
-    }
+	@Override
+	public int evaluation() {
+		int eval = 0;
+		if (this.solved()){
+			return Integer.MAX_VALUE;
+		}
+		for (int i = 0; i < conflicts.length; i++) {
+			eval += conflicts[i];
+		}
+		return conflicts.length*conflicts.length - eval;
+	}
+
+	@Override
+	public List<Integer> leastConflictedNeighbours(int chosenIndex) {
+		int min = Integer.MAX_VALUE;
+		List<Integer> list = new ArrayList<Integer>();
+		int [] tempConflicts = new int[k];
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j < k; j++) {
+				if (j!=chosenIndex) {
+					if (i==queens[j]) {
+						tempConflicts[i]++;
+					}
+					int offsetPos = Math.abs(j-chosenIndex);
+					if (i==queens[j]+offsetPos) {
+						tempConflicts[i]++;
+					}
+					if (i==queens[j]-offsetPos) {
+						tempConflicts[i]++;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < k; i++) {
+			if (tempConflicts[i] < min) {
+				min = tempConflicts[i];
+			}
+		}
+		for (int i = 0; i < k; i++) {
+			if(tempConflicts[i] == min) {
+				list.add(i);
+			}
+		}
+		if (list.contains(queens[chosenIndex])&&(list.size()!=1)) {
+			list.remove(list.indexOf(queens[chosenIndex]));
+		}
+		return list;
+	}
+
+	@Override
+	public void change(int chosenIndex, int chosenNeighbour) {
+		addConflict(chosenIndex, -1);
+		queens[chosenIndex] = chosenNeighbour;
+		addConflict(chosenIndex, 1);
+	}
+
+	@Override
+	public void display() {
+		System.out.println(this.toString());
+	}
+
 }
